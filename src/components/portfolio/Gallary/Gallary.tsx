79279d4e4a817c1b73d1364da6f36b3e55d1c93f
TharakaJayz <tharakaprabhath300@gmail.com>
2024-09-16 10:38:55 +0530

    fix single image gallary display bug

--- src/components/portfolio/Gallary/Gallary.tsx
+++ src/components/portfolio/Gallary/Gallary.tsx
@@ -1,17 +1,24 @@
-
-import "./Gallary.css"
+import "./Gallary.css";
 type Props = {
-    images: Array<string>
-}
+  images: Array<string>;
+};
 
 const Gallary = (props: Props) => {
-    return (
-        <div className='gallary'>
-        { props.images.length >0 &&  (props.images.map((image: any,index) => {
-                        return <img src={image} alt='' className='portfolio_gallary_img' key={index} />
-                    }))}
-        </div>
-    )
-}
+  return (
+    <div className="gallary">
+      {props.images.length > 0 &&
+        props.images.map((image: any, index) => {
+          return (
+            <img
+              src={image}
+              alt=""
+              className="portfolio_gallary_img"
+              key={index}
+            />
+          );
+        })}
+    </div>
+  );
+};
 
-export default Gallary
+export default Gallary;
